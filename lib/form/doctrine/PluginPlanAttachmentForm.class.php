dd8f03a8b3779df80fde10f7eb50010433289d81
Gilles Doge <gilles@antistatique.net>
2011-07-06 10:51:09 +0200

    Add type 'plan'

---
+++ lib/form/doctrine/PluginPlanAttachmentForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * PluginPlanAttachment form.
+ *
+ * @package    form
+ * @subpackage ImageAttachment
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+abstract class PluginPlanAttachmentForm extends PluginImageAttachmentForm
+{
+  public function setUp()
+  {
+    parent::setUp();
+  }
+}
